94384227f32f708e36a8da1a66d9517db0dec27e
Roj <chanoktrue@me.com>
2026-09-10 22:24:50 +0700

    Add hello HTTP API

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello form go on Omarchy!")
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		fmt.Fprintln(w, "Hello from Go on Omarchy!")
+	})
+
+	fmt.Println("HTTP server listening on http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println(err)
+	}
 }
